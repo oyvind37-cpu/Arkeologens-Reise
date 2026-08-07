9db426041cd53837f223af6edb9ddc6e8f06efb0
oyvind37-cpu <oyvind37@gmail.com>
2026-08-07 16:10:15 +0200

    Delete dialogue.js

--- dialogue.js
+++
@@ -1,4 +0,0 @@
-export class Dialogue{
-constructor(box,text){this.box=box;this.text=text;this.timer=null}
-say(t,d=4200){clearTimeout(this.timer);this.text.textContent=t;this.box.classList.remove("hidden");this.timer=setTimeout(()=>this.box.classList.add("hidden"),d)}
-}
