6582d028bf4738c214590ea0d69de747ad77f10d
oyvind37-cpu <oyvind37@gmail.com>
2026-08-07 16:08:09 +0200

    Delete input.js
    ok

--- input.js
+++
@@ -1,5 +0,0 @@
-export class Input{
-constructor(){this.state={left:false,right:false,jump:false,interact:false};this.pressed=new Set()}
-bindButton(btn,a){const d=e=>{e.preventDefault();this.state[a]=true;this.pressed.add(a)};const u=e=>{e.preventDefault();this.state[a]=false};btn.addEventListener("pointerdown",d);["pointerup","pointercancel","pointerleave"].forEach(x=>btn.addEventListener(x,u))}
-consume(a){if(!this.pressed.has(a))return false;this.pressed.delete(a);return true}
-}
