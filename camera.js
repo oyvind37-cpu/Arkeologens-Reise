97892cefef21022101bccae90bb3df64c0e58de4
oyvind37-cpu <oyvind37@gmail.com>
2026-08-07 16:09:57 +0200

    Delete camera.js

--- camera.js
+++
@@ -1,4 +0,0 @@
-export class Camera{
-constructor(viewWidth,worldWidth){this.x=0;this.viewWidth=viewWidth;this.worldWidth=worldWidth}
-update(targetX){const d=targetX-this.viewWidth*.38;this.x+=(d-this.x)*.12;this.x=Math.max(0,Math.min(this.worldWidth-this.viewWidth,this.x))}
-}
